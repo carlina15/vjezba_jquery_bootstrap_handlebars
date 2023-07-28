223691b855a0b848ebbcd0e07b285da9a5eca12a
Karla Vujevic <karla_vu@icloud.com>
2023-07-28 20:05:24 +0200

    Added jquery example

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const source = document.getElementById("entry-template").innerHTML;
-const template = Handlebars.compile(source);
+$(document).ready(async function() {
+  try {
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon-color/yellow"
+    );
+    const data = await response.json();
+    const source = $("#pokemon-list").html();
+    const template = Handlebars.compile(source);
+    const context = {
+      pokemon: data.pokemon_species.slice(0, 20),
+      title: "Pokemons",
+    };
+    const html = template(context);
 
-const context = { title: "My New Post", body: "This is my first post!", imgURL:"https://robohash.org/matej" };
-const html = template(context);
-
-console.log(html)
-
-document.querySelector("body").innerHTML = html
+    $("#result").html(html);
+  } catch (error) {
+    console.log(error);
+  }
+});
